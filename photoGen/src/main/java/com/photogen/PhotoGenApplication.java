aa13796fda5cfd647fe2c92c3f7bfe9961cf7815
obaraten64 <obaraten64@gmail.com>
2026-03-14 11:29:52 +0200

    Init commit

---
+++ photoGen/src/main/java/com/photogen/PhotoGenApplication.java
@@ -0,0 +1,13 @@
+package com.photogen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoGenApplication.class, args);
+    }
+
+}
---
+++ photoGen/src/test/java/com/photogen/PhotoGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photogen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
